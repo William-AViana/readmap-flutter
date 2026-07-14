69cd61133b844efa8fd6a7ce11b295bbae024752
William-AViana <willdev.viana@gmail.com>
2026-07-13 22:05:57 -0300

    sintaxe function dart

---
+++ sintaxe/main.dart
@@ -0,0 +1,12 @@
+// This is a simple Dart program that prints "Hello World" to the console.
+void main(){
+  print("Hello World");
+}
+
+// this is a simple comment in Dart. It is used to explain the code and make it more readable.
+
+/* This is a multi-line comment in Dart. 
+It can span multiple lines and is used to provide 
+more detailed explanations or documentation for the code.
+*/
+/// this is a comment that is used to generate documentation.

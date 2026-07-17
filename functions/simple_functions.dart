d905adf3904919e63966cb406c174bdb706e1156
William-AViana <willdev.viana@gmail.com>
2026-07-16 22:05:55 -0300

    simple functions

---
+++ functions/simple_functions.dart
@@ -0,0 +1,12 @@
+// This function prints "Hello, World!" to the console.
+void speakHello() {
+  print("Hello, World!");
+}
+
+//
+int addNumbers(int a, int b) {
+  return a + b;
+}
+
+// Arrow function syntax for a simple addition operation.
+int sum (int a, int b) => a + b;

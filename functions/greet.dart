f6c29764a80af19a9dffb949fafe7c9ea16ccc0b
William-AViana <willdev.viana@gmail.com>
2026-07-16 22:04:43 -0300

    update file

--- functions/greet.dart
+++ functions/greet.dart
@@ -1,12 +1,3 @@
-void speakHello() {
-  print("Hello, World!");
-}
-
-int addNumbers(int a, int b) {
-  return a + b;
-}
-
-
 // This function takes a name as input and returns a greeting message. If the name contains "World", it returns a specific greeting. Otherwise, it returns a generic greeting.
 String greet(String name) {
   if (name.contains("World")) {

e222cf42c012f7faeaff76c731ed9f6c063b9506
William-AViana <willdev.viana@gmail.com>
2026-07-16 21:54:50 -0300

    create file and learning loops

---
+++ loops/for.dart
@@ -0,0 +1,10 @@
+// Example of a for loop
+for (int i = 0; i < 5; i++) {
+  print("Iteration: $i");
+}
+
+// Example of a for-in loop on a list
+List<String> fruits = ["Apple", "Banana", "Cherry"];
+for (String fruit in fruits) {
+  print("Fruit: $fruit");
+}
---
+++ loops/while.dart
@@ -0,0 +1,11 @@
+int i = 0;
+
+while (i < 5) {
+  print("Iteration: $i");
+  i++;
+}
+
+do {
+  print("Iteration: $i");
+  i++;
+} while (i < 5);

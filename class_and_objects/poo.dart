2788fe12b6c71728e0aa8cfd19c0c2c9da2dd8ac
William-AViana <willdev.viana@gmail.com>
2026-07-21 21:43:59 -0300

    leaning poo

---
+++ class_and_objects/poo.dart
@@ -0,0 +1,16 @@
+// This is a simple class definition for a car. It currently does not have any properties or methods defined.
+class Car{
+
+}
+
+
+void main() {
+  // Creating an instance of the Car class
+  Car myCar = Car();
+
+  Car mySecondCar = Car();
+  
+  // Printing a message to indicate that the car object has been created
+  print("A new car object has been created: $myCar");
+  print("A new car object has been created: $mySecondCar");
+}

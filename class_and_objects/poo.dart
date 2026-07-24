47c21941558068d1df0343781de7cfe4e1398f14
William-AViana <willdev.viana@gmail.com>
2026-07-23 21:42:06 -0300

    increment class carwith methods

--- class_and_objects/poo.dart
+++ class_and_objects/poo.dart
@@ -1,6 +1,19 @@
 // This is a simple class definition for a car. It currently does not have any properties or methods defined.
 class Car{
 
+  String model; 
+
+  int speed;
+
+  void start() {
+    print("The car has started.");
+  } 
+
+  // Acelerate method to increase the speed of the car by a given increment. It updates the speed property and prints the current speed.
+  void accelerate(int increment) {
+    speed += increment;
+    print("The car is accelerating. Current speed: $speed km/h");
+  } 
 }
 
 

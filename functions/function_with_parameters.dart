7d8dc3df6156b1bc4e1b7162df0b4ec98dc29fde
William-AViana <willdev.viana@gmail.com>
2026-07-27 21:49:04 -0300

    update sintaxe with function

--- functions/function_with_parameters.dart
+++ functions/function_with_parameters.dart
@@ -10,7 +10,16 @@ String greet(String name, [String? title]) {
 // Function with named parameters
 String user({required String name, required int age}) {
   return "My name is $name and I am $age years old.";
-} 
+}
 
 // Example usage of the user function with named parameters
-user({name: "Alice", age: 30}); 
+void main() {
+  print(greet("Alice")); // Output: Hello, Alice!
+  print(greet("Bob", "Mr.")); // Output: Hello, Mr. Bob!
+  print(
+    user(name: "Alice", age: 30),
+  ); // Output: My name is Alice and I am 30 years old.
+  print(
+    user(name: "Bob", age: 25),
+  ); // Output: My name is Bob and I am 25 years old.
+}
